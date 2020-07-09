9f45dc1e7c6c757050d8e896fcd260655df25418
meha <meha.garg@gotinder.com>
2020-07-09 15:51:31 -0700

    Firebase

---
+++ cardstack/firebaseapp/src/main/java/com/example/firebaseapp/FAQActivity.kt
@@ -0,0 +1,11 @@
+package com.example.firebaseapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FAQActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_f_a_q)
+    }
+}
